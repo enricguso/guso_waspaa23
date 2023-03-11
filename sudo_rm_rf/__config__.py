5beeadef1d55ca5b1ebce660f95b88a3ef6e6ec0
Enric <enricguso@gmail.com>
2023-03-11 20:51:02 +0100

    added the train scripts

---
+++ sudo_rm_rf/__config__.py
@@ -0,0 +1,18 @@
+WSJ0_MIX_2_8K_PATH = '/home/ubuntu/Data/WSJ0/wsj0-mix/2speakers/wav8k'
+ESC50_DOWNLOADED_P = '/home/ubuntu/Data/ESC-50-master'
+WSJ_MIX_HIERARCHICAL_P = '/home/ubuntu/Data/WSJ0_mix_partitioned/'
+ESC50_HIERARCHICAL_P = '/home/ubuntu/Data/ESC50_partitioned/'
+WHAM_ROOT_PATH = '/home/ubuntu/Data/wham'
+
+#WHAMR_ROOT_PATH = '/home/ubuntu/Data/microson_v1'
+WHAMR_ROOT_PATH = '/home/ubuntu/Data/whamr'
+
+#WHAMR_ROOT_PATH = '/home/ubuntu/Data/whamr_sameT60noRISM_broadband'
+LIBRI2MIX_ROOT_PATH = '/home/ubuntu/Data/LibriMix/Libri2Mix'
+MUSDB_ROOT_PATH = '/home/ubuntu/Data/Music/MUSDB18'
+MUSDBWAV_ROOT_PATH = '/home/ubuntu/Data/Music/MUSDB18wav'
+MUSDBWAV8K_ROOT_PATH = '/home/ubuntu/Data/Music/MUSDB18wav8k'
+FUSS_ROOT_PATH = '/home/ubuntu/Data/Fuss/fuss_dev/ssdata'
+
+#API_KEY = '4MedNCb6cMj06laY9jFn5pDpA'
+API_KEY = 'O1LMM1TV4H9vbT0CKTVlbRk3T'
---
+++ sudo_rm_rf/m1_alldata_normal.sh
@@ -0,0 +1,7 @@
+exp_name=m1_alldata_normal
+mkdir /home/ubuntu/Data/enric_models/$exp_name
+nohup python run_microson_v1.py \
+--separation_task enh_noisyreverberant --n_train 220701 --n_test 2385 --n_val 2408 \
+--out_channels 512 --in_channels 256 --enc_kernel_size 21 --num_blocks 16 -cad 0 -bs 2 --divide_lr_by 3. --upsampling_depth 5 \
+--patience 8 -fs 16000 -tags $exp_name --project_name microson_v1 --zero_pad --clip_grad_norm 5.0 --normalize_online --online_mix \
+--checkpoints_path /home/ubuntu/Data/enric_models/$exp_name --save_checkpoint_every 1 --n_epochs 50 #> $exp_name.txt
